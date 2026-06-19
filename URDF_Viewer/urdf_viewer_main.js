557f80f12cdde109b5a27447175dd035ef741f74
Artemio Araya Day <78178128+artemioadaysolvers@users.noreply.github.com>
2026-06-18 20:57:32 -0400

    Delete URDF_Viewer directory

--- URDF_Viewer/urdf_viewer_main.js
+++
@@ -1,4 +0,0 @@
-// AutoMind URDF+ modular viewer alias entrypoint.
-// Directly re-exports render(opts) from the main URDF+ module.
-export { render } from './urdfplus_viewer_main.js';
-export { default } from './urdfplus_viewer_main.js';
